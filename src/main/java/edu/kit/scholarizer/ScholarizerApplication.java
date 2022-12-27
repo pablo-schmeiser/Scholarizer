35235f7290bed1fd2b297d1e03ddec1fadb5959b
TiWo778 <64957774+TiWo778@users.noreply.github.com>
2022-12-27 14:07:24 +0100

    changed package structure and added layeredArchitecture.uxf

--- src/main/java/edu/kit/scholarizer/ScholarizerApplication.java
+++ src/main/java/edu/kit/scholarizer/ScholarizerApplication.java
@@ -1,4 +1,4 @@
-package com.kit.ces.scholarizer;
+package edu.kit.scholarizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
